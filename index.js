8f8aff0ac1ea4d32523a0efb021bc4d143662a75
srwatlanta <shawnrwheeler@gmail.com>
2019-08-27 18:09:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.substring(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let x = string.split(" ")
+    for(let i = 0; i < x.length; i++){
+      if(i === 0) {
+        result.push(this.capitalize(x[i]))
+      }else{
+        if(words.includes(x[i])){
+          result.push(x[i])
+        }else {
+          result.push(this.capitalize(x[i]))
+        }
+      }
+    }
+    return result.join(" ")
+  }
 }
